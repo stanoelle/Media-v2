a7fede0de0945549d6fcd5d843309d9444079a9b
stanoelle <118582341+stanoelle@users.noreply.github.com>
2023-07-22 21:20:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,11 +30,22 @@ class Bot(Client):
             sleep_threshold=5,
         )
     async def start(self):
+        b_users, b_chats = await db.get_banned()
+        temp.BANNED_USERS = b_users
+        temp.BANNED_CHATS = b_chats
         await super().start()
+        await Media.ensure_indexes()
+        me = await self.get_me()
+        temp.ME = me.id
+        temp.U_NAME = me.username
+        temp.B_NAME = me.first_name
+        self.username = '@' + me.username
+        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        logging.info(LOG_STR)
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, 80).start()
+        await web.TCPSite(app, bind_address, 8080).start()
 
     
 
